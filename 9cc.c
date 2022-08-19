e46605cbbaddc61ad45835251648e6e4f35338a5
RIRU <riruoda@gmail.com>
2022-08-19 18:54:45 +0900

    create compiler that can add and subtract

--- 9cc.c
+++ 9cc.c
@@ -2,12 +2,35 @@
 #include <stdlib.h>
 
 #define STREAM stdout
+#define ERROR stderr
 
-int main(const int argc, const char* argv[]){
+int main(int argc, char* argv[]){
+    if(argc != 2){
+        fprintf(ERROR, "引数の個数が正しくありません\n");
+        return EXIT_FAILURE;
+    }
+    char * p = argv[1];
+
     fprintf(STREAM, ".intel_syntax noprefix\n");
     fprintf(STREAM, ".global main\n");
     fprintf(STREAM, "main:\n");
-    fprintf(STREAM, "\tmov rax, %d\n", atoi(argv[1]));
+    fprintf(STREAM, "\tmov rax, %ld\n", strtol(p, &p, 10));
+
+    while(*p){
+        if(*p == '+'){
+            p++;
+            fprintf(STREAM, "\tadd rax, %ld\n", strtol(p, &p, 10));
+            continue;
+        }else if(*p == '-'){
+            p++;
+            fprintf(STREAM, "\tsub rax, %ld\n", strtol(p, &p, 10));
+            continue;
+        }else{
+            fprintf(ERROR, "予期しない入力です\n");
+            return 1;
+        }
+    }
+
     fprintf(STREAM, "\tret\n");
     return 0;
 
