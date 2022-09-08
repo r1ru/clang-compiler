3f6197be87cdc9af7bef9254aa53601a7ad537fc
RIRU <riruoda@gmail.com>
2022-09-08 22:22:57 +0900

    fix bug in vec_push

--- utils.c
+++ utils.c
@@ -12,7 +12,7 @@ void vec_push(Vector* vp, void* elem){
     if(vp -> len == vp -> capacity){
         // 容量がいっぱいなら2倍に拡張。
         vp -> capacity *= 2; 
-        vp -> data = realloc(vp -> data, 16 * sizeof(void*));
+        vp -> data = realloc(vp -> data, vp -> capacity * sizeof(void*));
     }
     vp -> data[vp -> len] = elem; // 0番目の要素はどうなる?
     vp -> len ++;
